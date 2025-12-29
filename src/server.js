384724dc45b82704fc0fac0ab6c99f7db3aceab3
escrimeuse <cathryn.griffiths@gmail.com>
2025-12-29 11:57:24 -0500

    playing around some more

--- src/server.js
+++ src/server.js
@@ -1,32 +1,30 @@
 import {createServer} from 'node:http';
 import {httpServerHandler} from 'cloudflare:node'
 import {readFileSync} from 'node:fs';
-import path from 'node:path';
 
-const ALLOWED_PATHS = ['/', '/index.html', '/blah'];
+// We import our HTML files so they can be part of the worker bundle and be served directly.
+// This isn't really necessary, we could specify these as static assets in the wrangler.jsonc
+// file, but I'm just making this complicated because I'm just playing around
+import fourOhFour from './404.html';
+import index from './index.html';
+
+const ALLOWED_PATHS = ['/', '/index.html'];
 
 const server = createServer((req, res) => {
     if (ALLOWED_PATHS.includes(req.url)) {
-        const htmlPath = path.join('src/', req.url === '/' ? '/index.html' : req.url);
-
         try { 
-            const fileContent = readFileSync(htmlPath, 'utf-8');
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
-            res.end(fileContent);
+            res.end(index);
         } catch (error) {
             handleInternalServerError(res, error);
         }
     } else {
         // General 404 handling
-
-        const htmlPath = path.join('src/', '404.html');
-
         try {
-            const fileContent = readFileSync(htmlPath, 'utf-8');
             res.statusCode = 404;
             res.setHeader('Content-Type', 'text/html');
-            res.end(fileContent);
+            res.end(fourOhFour);
         } catch (error) {
             handleInternalServerError(res, error);
         }
