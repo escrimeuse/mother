ac408f33c7a906280994d9ff26f498bc1ff11ecc
escrimeuse <cathryn.griffiths@gmail.com>
2025-12-29 10:30:28 -0500

    Update gitignore

--- src/server.js
+++ src/server.js
@@ -1,45 +1,45 @@
 import {createServer} from 'node:http';
-import {handleAsNodeRequest, httpServerHandler} from 'cloudflare:node'
+import {httpServerHandler} from 'cloudflare:node'
 import {readFileSync} from 'node:fs';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const hostname = '127.0.0.1';
-const port = 3000;
 
 const ALLOWED_PATHS = ['/', '/index.html', '/blah'];
-
 
 const server = createServer((req, res) => {
     if (ALLOWED_PATHS.includes(req.url)) {
         const htmlPath = path.join('src/', req.url === '/' ? '/index.html' : req.url);
-        readFileSync(htmlPath, (error, data) => {
-            if (error) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Internal Server Error\n');
-                return;
-            }
 
+        try { 
+            const fileContent = readFileSync(htmlPath, 'utf-8');
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        });
+            res.end(fileContent);
+        } catch (error) {
+            handleInternalServerError(res, error);
+        }
     } else {
-        const htmlPath = path.join('src/', '404.html');
-        readFileSync(htmlPath, (error, data) => {
-            res.statusCode = 404;
-            if (error) {
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Not Found\n');
-                return;
-            } 
-            res.end(data);
-        });
+        // General 404 handling
 
+        const htmlPath = path.join('src/', '404.html');
+
+        try {
+            const fileContent = readFileSync(htmlPath, 'utf-8');
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(fileContent);
+        } catch (error) {
+            handleInternalServerError(res, error);
+        }
        
     }
 });
+
+function handleInternalServerError(res, error) {
+    console.log("Error reading file:", error);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Internal Server Error\n');
+}
 
 server.listen(8080);
 
